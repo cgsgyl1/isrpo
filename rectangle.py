b4585ab2670c8c407b7982153331a4944607e7c9
cgsgyl1 <ya2006lev@gmail.com>
2025-10-23 23:17:22 +0300

    rectangle.py: documentation added

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,23 @@
 
 def area(a, b):
+    '''
+    Calculating rectangle's square function.
+        ARGUMENTS:
+            * a (int) - rectangle's height
+            * b (int) - rectangle's width
+        RETURNS:
+            * result square
+    '''
     return a * b
 
 
 def perimeter(a, b):
+    '''
+    Calculating rectangle's perimeter function.
+        ARGUMENTS:
+            * a (int) - rectangle's height
+            * b (int) - rectangle's width
+        RETURNS:
+            * result perimeter
+    '''
     return 2 * (a + b)
