5852d71c462cbf3f191b42602bf783e99633c77c
cgsgyl1 <ya2006lev@gmail.com>
2025-10-23 23:37:33 +0300

    triangle.py: documentation added

--- triangle.py
+++ triangle.py
@@ -1,7 +1,24 @@
 
 def area(a, h):
+    '''
+    Calculating triangle's area function.
+        ARGUMENTS:
+            * a (int) - triangle's one side's lenght
+            * h (int) - triangle's height to this side
+        RETURNS:
+            * result area
+    '''
     return a * h / 2
 
 
 def perimeter(a, b, c):
+    '''
+    Calculating triangle's perimeter function.
+        ARGUMENTS:
+            * a (int) - triangle's first side's lenght
+            * b (int) - triangle's second side's lenght
+            * c (int) - triangle's third side's lenght
+        RETURNS:
+            * result perimeter
+    '''
     return a + b + c
