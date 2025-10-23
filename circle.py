be6d38eb69f8bc07babf530f5c9b57caaa2ec960
cgsgyl1 <ya2006lev@gmail.com>
2025-10-23 23:13:02 +0300

    circle.py: documentation added

--- circle.py
+++ circle.py
@@ -2,9 +2,23 @@ import math
 
 
 def area(r):
+    '''
+    Calculating circle's square function.
+        ARGUMENTS:
+            * r (int) - circle's radius
+        RETURNS:
+            * result square
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Calculating circle's perimeter function.
+        ARGUMENTS:
+            * r (int) - circle's radius
+        RETURNS:
+            * result perimeter
+    '''
     return 2 * math.pi * r
 
