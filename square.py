02472e25bba9904a6b2a504a6c9de61d17cd2a6e
cgsgyl1 <ya2006lev@gmail.com>
2025-10-23 23:20:42 +0300

    square.py: documentation added

--- square.py
+++ square.py
@@ -1,7 +1,21 @@
 
 def area(a):
+    '''
+    Calculating area's square function.
+        ARGUMENTS:
+            * a (int) - area's lenght
+        RETURNS:
+            * result square
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    Calculating area's perimeter function.
+        ARGUMENTS:
+            * a (int) - area's lenght
+        RETURNS:
+            * result perimeter
+    '''
     return 4 * a
